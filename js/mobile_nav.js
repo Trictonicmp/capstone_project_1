6f47edb4db880b4d6ed9463995b9326b1eb095cc
Trictonicmp <code_shadow@hotmail.com>
2022-04-12 18:46:14 -0500

    Add nav active functionallity

--- js/mobile_nav.js
+++ js/mobile_nav.js
@@ -4,3 +4,23 @@ function toggleNav() {
 
 document.getElementById('mobile-btn').addEventListener('click', toggleNav);
 document.getElementById('close-nav-btn').addEventListener('click', toggleNav);
+
+
+let navLi = document.querySelectorAll('#nav-list li');
+let sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 800;
+    if (scrollY >= sectionTop ) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.children[0].classList.remove("active");
+    if (li.children[0].classList.contains(current)) {
+      li.children[0].classList.add("active");
+    }
+  });
+};
